8432e6fb1347ea35fc2dac41d6964da181ccf456
jakubhamer <jakub.hamer@unseen.is>
2018-06-19 00:30:26 +0200

    version 1.01

--- src/main.c
+++ src/main.c
@@ -1,3 +1,7 @@
+/**
+ * @author ngiersz, LiLatee, jakubhamer
+ * @version 1.01 18/06/2018
+ */
 #include "stm32f4xx.h"
 #include "stm32f4_discovery.h"
 #include <stdio.h>
@@ -6,9 +10,8 @@
 #include "stepper_motor.h"
 #include "letters.h"
 
-int length;
 int timeToWrite = 0;
-
+int counter = 0;
 char tab[30];
 void memoryClear();
 int main(void) {
@@ -20,23 +23,22 @@ int main(void) {
 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_SYSCFG, ENABLE);
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
 
-	GPIO_InitTypeDef sterownik1;
-	sterownik1.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_1 | GPIO_Pin_2 | GPIO_Pin_3;
-	sterownik1.GPIO_Mode = GPIO_Mode_OUT;
-	sterownik1.GPIO_OType = GPIO_OType_PP;
-	sterownik1.GPIO_Speed = GPIO_Speed_100MHz;
-	sterownik1.GPIO_PuPd = GPIO_PuPd_NOPULL;
-	GPIO_Init(GPIOA, &sterownik1);
+	GPIO_InitTypeDef driver1;
+	driver1.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_1 | GPIO_Pin_2 | GPIO_Pin_3;
+	driver1.GPIO_Mode = GPIO_Mode_OUT;
+	driver1.GPIO_OType = GPIO_OType_PP;
+	driver1.GPIO_Speed = GPIO_Speed_100MHz;
+	driver1.GPIO_PuPd = GPIO_PuPd_NOPULL;
+	GPIO_Init(GPIOA, &driver1);
 
-	GPIO_InitTypeDef sterownik2;
-	sterownik2.GPIO_Pin = GPIO_Pin_8 | GPIO_Pin_9 | GPIO_Pin_10 | GPIO_Pin_11;
-	sterownik2.GPIO_Mode = GPIO_Mode_OUT;
-	sterownik2.GPIO_OType = GPIO_OType_PP;
-	sterownik2.GPIO_Speed = GPIO_Speed_100MHz;
-	sterownik2.GPIO_PuPd = GPIO_PuPd_NOPULL;
-	GPIO_Init(GPIOD, &sterownik2);
+	GPIO_InitTypeDef driver2;
+	driver2.GPIO_Pin = GPIO_Pin_8 | GPIO_Pin_9 | GPIO_Pin_10 | GPIO_Pin_11;
+	driver2.GPIO_Mode = GPIO_Mode_OUT;
+	driver2.GPIO_OType = GPIO_OType_PP;
+	driver2.GPIO_Speed = GPIO_Speed_100MHz;
+	driver2.GPIO_PuPd = GPIO_PuPd_NOPULL;
+	GPIO_Init(GPIOD, &driver2);
 
-	// konfiguracja linii Rx i Tx
 	GPIO_InitTypeDef GPIO_InitStructure;
 	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10 | GPIO_Pin_11;
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
@@ -48,7 +50,6 @@ int main(void) {
 	GPIO_PinAFConfig(GPIOC, GPIO_PinSource10, GPIO_AF_USART3);
 	GPIO_PinAFConfig(GPIOC, GPIO_PinSource11, GPIO_AF_USART3);
 
-	// UART
 	NVIC_InitTypeDef NVIC_InitStructure;
 	USART_ITConfig(USART3, USART_IT_RXNE, ENABLE);
 	NVIC_InitStructure.NVIC_IRQChannel = USART3_IRQn;
@@ -64,10 +65,9 @@ int main(void) {
 	USART_InitStructure.USART_StopBits = USART_StopBits_1;
 	USART_InitStructure.USART_Parity = USART_Parity_No;
 	USART_InitStructure.USART_HardwareFlowControl =
-			USART_HardwareFlowControl_None;
+	USART_HardwareFlowControl_None;
 	USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
 
-	// konfiguracja
 	USART_Init(USART3, &USART_InitStructure);
 	USART_Cmd(USART3, ENABLE);
 
@@ -75,10 +75,9 @@ int main(void) {
 	config_PWM();
 	setSpeed(300);
 	pen_up();
-	Delay(500);
 	pen_down();
-	Delay(500);
 	pen_up();
+
 	while (1) {
 		if (timeToWrite) {
 			draw_text(tab);
@@ -96,23 +95,19 @@ memoryClear() {
 }
 
 uint8_t usartGetChar(void) {
-	// czekaj na odebranie danych
 	while (USART_GetFlagStatus(USART3, USART_FLAG_RXNE) == RESET)
 		;
-
 	return USART_ReceiveData(USART3);
 }
-
 void USART3_IRQHandler(void) {
-	// sprawdzenie flagi zwiazanej z odebraniem danych przez USART
 	if (USART_GetITStatus(USART3, USART_IT_RXNE) != RESET) {
-		char l1 = usartGetChar();
-		char l2 = usartGetChar();
-
-		length = (l1 - 48) * 10 + (l2 - 48);
-
-		for (int i = 0; i < length; i++)
-			tab[i] = usartGetChar();
+		counter = 0;
+		tab[0] = usartGetChar();
+		while (tab[counter] != '@') {
+			counter++;
+			tab[counter] = usartGetChar();
+		}
+		tab[counter] = '\0';
+		timeToWrite = 1;
 	}
-	timeToWrite = 1;
 }
--- src/servo_motor.h
+++ src/servo_motor.h
@@ -7,11 +7,7 @@
 
 void config_PWM(void) {
 
-	// Structures for configuration
-
-	// TIM2 Clock Enable
 	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE);
-	// GPIOB Clock Enable
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB, ENABLE);
 
 	GPIO_InitTypeDef GPIO_InitStructure;
@@ -24,7 +20,6 @@ void config_PWM(void) {
 
 	GPIO_PinAFConfig(GPIOB, GPIO_PinSource11, GPIO_AF_TIM2);
 
-// timer 4
 	TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure;
 	TIM_TimeBaseStructure.TIM_Period = 20000 - 1;
 	TIM_TimeBaseStructure.TIM_Prescaler = 85 - 1;
@@ -33,11 +28,10 @@ void config_PWM(void) {
 
 	TIM_TimeBaseInit(TIM2, &TIM_TimeBaseStructure);
 
-// PWM
 	TIM_OCInitTypeDef TIM_OCInitStructure;
 	TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_PWM1;
 	TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
-	TIM_OCInitStructure.TIM_Pulse = 0;                     // Initial duty cycle
+	TIM_OCInitStructure.TIM_Pulse = 0;
 	TIM_OCInitStructure.TIM_OCPolarity = TIM_OCPolarity_High;
 
 	TIM_OC4Init(TIM2, &TIM_OCInitStructure);
